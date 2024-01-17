687b0ccde508f25be7cbb145f3dbacf372a24465
bataller <ebataller@inscastellet.cat>
2024-01-17 16:21:28 +0100

    Evolució Tasca 7

--- Tasca 7/Tasca_7_Bataller.py
+++ Tasca 7/Tasca_7_Bataller.py
@@ -156,7 +156,7 @@ print(miles_gallon_to_liters_100km(60.3))
 print(miles_gallon_to_liters_100km(31.4))
 print(miles_gallon_to_liters_100km(23.5))
 
-'''
+
 separador(5)
 
 def liters_100km_to_miles_gallon(liters):
@@ -175,3 +175,24 @@ print(liters_100km_to_miles_gallon(10.))
 print(miles_gallon_to_liters_100km(60.3))
 print(miles_gallon_to_liters_100km(31.4))
 print(miles_gallon_to_liters_100km(23.5))
+
+
+separador(8)
+
+def decimal_a_binari(decimal):
+    binario = ''
+    while decimal // 2 != 0:
+        binario = str(decimal % 2) + binario
+        decimal = decimal // 2
+    return str(decimal) + binario
+
+
+numero = int(input('Introduce el número a convertir a binario: '))
+print(decimal_a_binari(numero))
+
+'''
+separador(9)
+
+
+
+
